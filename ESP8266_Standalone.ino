3894307c51661c367880116c0cb2e635eb678ade
Electron <31048230+JacksonDuvan@users.noreply.github.com>
2017-08-22 19:32:46 -0500

    Add files via upload

---
+++ ESP8266_Standalone.ino
@@ -0,0 +1,23 @@
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "f7f9ae187e554f739bf19ebdec214409"; //Auth Token de la app Blynk
+
+char ssid[] = "ARRIS-2692"; //Nombre del router 
+char pass[] = "contraseña"; //Contraseña del WiFi 
+
+void setup()
+{
+ 
+  Serial.begin(9600);//Comunicacion serial 
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
